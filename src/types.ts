a3756b966733cc0b730934bc2d1017c6166e260a
1234 <a1234@1234s-MacBook-Pro.local>
2023-02-26 09:06:11 +0100

    updates types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+// Original options from the process.argv
 export type RawOptions = {
   skipPrompts: boolean;
   initializeGit: boolean;
@@ -5,6 +6,7 @@ export type RawOptions = {
   installPackages: boolean;
 }
 
+// Processed original options which will be used by the CLI (final prepared version of options)
 export type Options = Omit<RawOptions, 'skipPrompts'> & {
   template: string;
 }
